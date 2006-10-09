77c5775a237fa4462bf0adfa4aa6fb3274b4d65f
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2006-10-09 06:33:47 +0000

    Base neuron class added
    git-svn-id: http://aforge.googlecode.com/svn/trunk@50 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Neuro/Neurons/Neuron.cs
@@ -0,0 +1,153 @@
+// AForge Neural Net Library
+//
+// Copyright © Andrew Kirillov, 2005-2006
+// andrew.kirillov@gmail.com
+//
+
+namespace AForge.Neuro
+{
+	using System;
+	using AForge;
+
+	/// <summary>
+	/// Base neuron class
+	/// </summary>
+	public abstract class Neuron
+	{
+		/// <summary>
+		/// Neuron's inputs count
+		/// </summary>
+		protected int		inputsCount = 0;
+
+		/// <summary>
+		/// Nouron's wieghts
+		/// </summary>
+		protected double[]	weights = null;
+
+		/// <summary>
+		/// Neuron's output value
+		/// </summary>
+		protected double	output = 0;				
+
+		/// <summary>
+		/// Random number generator
+		/// </summary>
+		/// 
+		/// <remarks>The generator is used for neuron's weights randomization</remarks>
+		/// 
+		protected static Random	rand = new Random( (int) DateTime.Now.Ticks );
+
+		/// <summary>
+		/// Random generator range
+		/// </summary>
+		/// 
+		/// <remarks>Sets the range of random generator. Affects initial values of neuron's weight.</remarks>
+		/// 
+		protected static DoubleRange randRange = new DoubleRange( 0.0, 1.0 );
+
+		/// <summary>
+		/// Random number generator
+		/// </summary>
+		/// 
+		/// <remarks>The property allows to initialize random generator with a custom seed. The generator is
+		/// used for neuron's weights randomization.</remarks>
+		/// 
+		public static Random RandGenerator
+		{
+			get { return rand; }
+			set
+			{
+				if ( value != null )
+				{
+					rand = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Random generator range
+		/// </summary>
+		public static DoubleRange RandRange
+		{
+			get { return randRange; }
+			set
+			{
+				if ( value != null )
+				{
+					randRange = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Neuron's inputs count
+		/// </summary>
+		public int InputsCount
+		{
+			get { return inputsCount; }
+		}
+
+		/// <summary>
+		/// Neuron's output value
+		/// </summary>
+		/// 
+		/// <remarks>The calculation way of neuron's output value is determined by inherited class.</remarks>
+		/// 
+		public double Output
+		{
+			get { return output; }
+		}
+
+		/// <summary>
+		/// Neuron's weights accessor
+		/// </summary>
+		/// 
+		/// <param name="index">Weight index</param>
+		/// 
+		/// <remarks>Allows to access neuron's weights.</remarks>
+		/// 
+		public double this[int index]
+		{
+			get { return weights[index]; }
+			set { weights[index] = value; }
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Neuron"/> class
+		/// </summary>
+		///
+		/// <param name="inputs">Neuron's inputs count</param> 
+		///
+		public Neuron( int inputs )
+		{
+		}
+
+		/// <summary>
+		/// Randomize weights 
+		/// </summary>
+		/// 
+		/// <remarks>Initialize neuron's weights with random values within the range specified
+		/// by <see cref="RandRange"/>.</remarks>
+		/// 
+		public virtual void Randomize( )
+		{
+			double d = randRange.Length;
+
+			// generate weights
+			for ( int i = 0; i < inputsCount; i++ )
+				weights[i] = rand.NextDouble( ) * d + randRange.Min;
+		}
+
+		/// <summary>
+		/// Compute output value of neuron
+		/// </summary>
+		/// 
+		/// <param name="input">Input vector</param>
+		/// 
+		/// <returns>Returns neuron's output value</returns>
+		/// 
+		/// <remarks>The actual neuron's output value is determined by inherited class.</remarks>
+		/// 
+		public abstract double Compute( double[] input );
+	}
+}
