6f5cae4d865edb1489492cf1737e72d1a8d155f1
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2007-02-14 08:01:56 +0000

    Adaptive Smooth and Simple Skeletonization added
    git-svn-id: http://aforge.googlecode.com/svn/trunk@142 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Imaging/Filters/Other/AdaptiveSmooth.cs
@@ -0,0 +1,176 @@
+// AForge Image Processing Library
+//
+// Copyright © Andrew Kirillov, 2005-2007
+// andrew.kirillov@gmail.com
+//
+// Found description in
+// "An Edge Detection Technique Using the Facet
+// Model and Parameterized Relaxation Labeling"
+// by Ioannis Matalas, Student Member, IEEE, Ralph Benjamin, and Richard Kitney
+//
+namespace AForge.Imaging.Filters
+{
+	using System;
+	using System.Drawing;
+	using System.Drawing.Imaging;
+
+	/// <summary>
+	/// Adaptive Smoothing - noise removal with edges preserving
+	/// </summary>
+    /// 
+    /// <remarks></remarks>
+    /// 
+    public class AdaptiveSmooth : FilterAnyToAnyNewSameSize
+	{
+		private double factor = 3.0;
+
+        /// <summary>
+        /// Factor value
+        /// </summary>
+        /// 
+        /// <remarks></remarks>
+        /// 
+		public double Factor
+		{
+			get { return factor; }
+			set { factor = value; }
+		}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdaptiveSmooth"/> class
+        /// </summary>
+        /// 
+		public AdaptiveSmooth( ) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdaptiveSmooth"/> class
+        /// </summary>
+        /// 
+        /// <param name="factor">Factor value</param>
+        /// 
+        public AdaptiveSmooth( double factor )
+		{
+			this.factor = factor;
+		}
+
+		/// <summary>
+		/// Process the filter on the specified image
+		/// </summary>
+		/// 
+		/// <param name="sourceData">Source image data</param>
+		/// <param name="destinationData">Destination image data</param>
+		/// 
+        protected override unsafe void ProcessFilter( BitmapData sourceData, BitmapData destinationData )
+        {
+            // get source image size
+            int width = sourceData.Width;
+            int height = sourceData.Height;
+            int widthM2 = width - 2;
+            int heightM2 = height - 2;
+
+            int stride = sourceData.Stride;
+            int pixelSize = ( sourceData.PixelFormat == PixelFormat.Format8bppIndexed ) ? 1 : 3;
+            int pixelSize2 = pixelSize * 2;
+            int offset = stride - width * pixelSize;
+
+            // gradient and weights
+            double gx, gy, weight, weightTotal, total;
+            // precalculated factor value
+            double f = -8 * factor * factor;
+
+            // do the job
+			byte * src = (byte *) sourceData.Scan0.ToPointer( ) + stride * 2;
+			byte * dst = (byte *) destinationData.Scan0.ToPointer( ) + stride * 2;
+
+            for ( int y = 2; y < heightM2; y++ )
+			{
+				src += 2 * pixelSize;
+				dst += 2 * pixelSize;
+
+				for ( int x = 2; x < widthM2; x++ )
+				{
+					for ( int i = 0; i < pixelSize; i++, src++, dst++ )
+					{
+						weightTotal	= 0;
+						total		= 0;
+
+						// original formulas for weight calculation:
+						// w(x, y) = exp( -1 * (Gx^2 + Gy^2) / (2 * factor^2) )
+						// Gx(x, y) = (I(x + 1, y) - I(x - 1, y)) / 2
+						// Gy(x, y) = (I(x, y + 1) - I(x, y - 1)) / 2
+						//
+						// here is a little bit optimized version
+
+						// x - 1, y - 1
+						gx		= src[- stride] - src[- pixelSize2 - stride];
+						gy		= src[- pixelSize] - src[- pixelSize - 2 * stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[- pixelSize - stride];
+						weightTotal += weight;
+
+						// x, y - 1
+						gx		= src[pixelSize - stride] - src[- pixelSize - stride];
+						gy		= *src - src[- 2 * stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[- stride];
+						weightTotal += weight;
+
+						// x + 1, y - 1
+						gx		= src[pixelSize2 - stride] - src[- stride];
+						gy		= src[pixelSize] - src[pixelSize - 2 * stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[pixelSize - stride];
+						weightTotal += weight;
+
+						// x - 1, y
+						gx		= *src - src[- pixelSize2];
+						gy		= src[- pixelSize + stride] - src[- pixelSize - stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[- pixelSize];
+						weightTotal += weight;
+
+						// x, y
+						gx		= src[pixelSize] - src[- pixelSize];
+						gy		= src[stride] - src[- stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * (*src);
+						weightTotal += weight;
+
+						// x + 1, y
+						gx		= src[pixelSize2] - *src;
+						gy		= src[pixelSize + stride] - src[pixelSize - stride];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[pixelSize];
+						weightTotal += weight;
+
+						// x - 1, y + 1
+						gx		= src[stride] - src[- pixelSize2 + stride];
+						gy		= src[- pixelSize + 2 * stride] - src[- pixelSize];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[-pixelSize + stride];
+						weightTotal += weight;
+
+						// x, y + 1
+						gx		= src[pixelSize + stride] - src[- pixelSize + stride];
+						gy		= src[2 * stride] - *src;
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[stride];
+						weightTotal += weight;
+
+						// x + 1, y + 1
+						gx		= src[pixelSize2 + stride] - src[stride];
+						gy		= src[pixelSize + 2 * stride] - src[pixelSize];
+						weight	= System.Math.Exp( ( gx * gx + gy * gy ) / f );
+						total	+= weight * src[pixelSize + stride];
+						weightTotal += weight;
+
+						// save destination value
+						*dst = ( weightTotal == 0.0 ) ? *src : (byte) System.Math.Min( total / weightTotal, 255.0 );
+					}
+				}
+				src += offset + 2 * pixelSize;
+				dst += offset + 2 * pixelSize;
+			}
+        }
+	}
+}
---
+++ Sources/Imaging/Filters/Other/SimpleSkeletonization.cs
@@ -0,0 +1,157 @@
+// AForge Image Processing Library
+//
+// Copyright © Andrew Kirillov, 2005-2007
+// andrew.kirillov@gmail.com
+//
+
+namespace AForge.Imaging.Filters
+{
+	using System;
+	using System.Drawing;
+	using System.Drawing.Imaging;
+
+	/// <summary>
+	/// Simple skeletonization filter
+	/// </summary>
+    /// 
+    /// <remarks></remarks>
+    /// 
+    public class SimpleSkeletonization : FilterGrayToGrayNewSameSize
+	{
+		private byte	bg = 0;
+		private byte	fg = 255;
+
+        /// <summary>
+        /// Background pixel color
+        /// </summary>
+        /// 
+        /// <remarks>Defalt value is 0 - black.</remarks>
+        /// 
+		public byte Background
+		{
+			get { return bg; }
+			set { bg = value; }
+		}
+
+        /// <summary>
+        /// Foreground pixel color
+        /// </summary>
+        /// 
+        /// <remarks>Defalt value is 255 - white.</remarks>
+        /// 
+		public byte Foreground
+		{
+			get { return fg; }
+			set { fg = value; }
+		}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleSkeletonization"/> class
+        /// </summary>
+		public SimpleSkeletonization( ) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleSkeletonization"/> class
+        /// </summary>
+        /// 
+        /// <param name="bg">Background pixel color</param>
+        /// <param name="fg">Foreground pixel color</param>
+        /// 
+        public SimpleSkeletonization( byte bg, byte fg )
+		{
+			this.bg = bg;
+			this.fg = fg;
+		}
+
+		/// <summary>
+		/// Process the filter on the specified image
+		/// </summary>
+		/// 
+		/// <param name="sourceData">Source image data</param>
+		/// <param name="destinationData">Destination image data</param>
+		/// 
+        protected override unsafe void ProcessFilter( BitmapData sourceData, BitmapData destinationData )
+        {
+            // get image size
+            int width   = destinationData.Width;
+            int height  = destinationData.Height;
+            int stride  = destinationData.Stride;
+            int offset  = stride - width;
+            int start;
+
+            // make destination image filled with background color
+            Win32.memset( destinationData.Scan0, bg, stride * height );
+
+            // do the job
+            byte* src0 = (byte*) sourceData.Scan0.ToPointer( );
+            byte* dst0 = (byte*) destinationData.Scan0.ToPointer( );
+            byte* src = src0;
+            byte* dst = dst0;
+
+            // horizontal pass
+
+            // for each line
+            for ( int y = 0; y < height; y++ )
+            {
+                start = -1;
+                // for each pixel
+                for ( int x = 0; x < width; x++, src++ )
+                {
+                    // looking for foreground pixel
+                    if ( start == -1 )
+                    {
+                        if ( *src == fg )
+                            start = x;
+                        continue;
+                    }
+
+                    // looking for non foreground pixel
+                    if ( *src != fg )
+                    {
+                        dst[start + ( ( x - start ) >> 1 )] = (byte) fg;
+                        start = -1;
+                    }
+                }
+                if ( start != -1 )
+                {
+                    dst[start + ( ( width - start ) >> 1 )] = (byte) fg;
+                }
+                src += offset;
+                dst += stride;
+            }
+
+            // vertical pass
+
+            // for each column
+            for ( int x = 0; x < width; x++ )
+            {
+                src = src0 + x;
+                dst = dst0 + x;
+
+                start = -1;
+                // for each row
+                for ( int y = 0; y < height; y++, src += stride )
+                {
+                    // looking for foreground pixel
+                    if ( start == -1 )
+                    {
+                        if ( *src == fg )
+                            start = y;
+                        continue;
+                    }
+
+                    // looking for non foreground pixel
+                    if ( *src != fg )
+                    {
+                        dst[stride * ( start + ( ( y - start ) >> 1 ) )] = (byte) fg;
+                        start = -1;
+                    }
+                }
+                if ( start != -1 )
+                {
+                    dst[stride * ( start + ( ( height - start ) >> 1 ) )] = (byte) fg;
+                }
+            }
+        }
+	}
+}
