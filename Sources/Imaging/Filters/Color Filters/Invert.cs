cec9ea0bd9635c22774bb5e22faf57a3bf50095f
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2007-08-24 04:28:42 +0000

    Some color/grayscale filters were updated to support partial filtering:  1) Invert.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@330 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Imaging/Filters/Color Filters/Invert.cs
+++ Sources/Imaging/Filters/Color Filters/Invert.cs
@@ -1,50 +1,59 @@
 // AForge Image Processing Library
+// AForge.NET framework
 //
-// Copyright © Andrew Kirillov, 2005-2006
+// Copyright © Andrew Kirillov, 2005-2007
 // andrew.kirillov@gmail.com
 //
 
 namespace AForge.Imaging.Filters
 {
-	using System;
-	using System.Drawing;
-	using System.Drawing.Imaging;
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
 
-	/// <summary>
-	/// Invert image
-	/// </summary>
-	/// 
-	/// <remarks>The filter inverts colored and grayscale images.</remarks> 
-	///
-	public sealed class Invert : FilterAnyToAny
-	{
-		/// <summary>
-		/// Process the filter on the specified image
-		/// </summary>
-		/// 
-		/// <param name="imageData">image data</param>
-		/// 
-		protected override unsafe void ProcessFilter( BitmapData imageData )
-		{
-			int width	= imageData.Width;
-			int height	= imageData.Height;
+    /// <summary>
+    /// Invert image.
+    /// </summary>
+    /// 
+    /// <remarks>The filter inverts colored and grayscale images.</remarks> 
+    ///
+    public sealed class Invert : FilterAnyToAnyPartial
+    {
+        /// <summary>
+        /// Process the filter on the specified image.
+        /// </summary>
+        /// 
+        /// <param name="imageData">Image data.</param>
+        /// <param name="rect">Image rectangle for processing by the filter.</param>
+        /// 
+        protected override unsafe void ProcessFilter( BitmapData imageData, Rectangle rect )
+        {
+            int pixelSize = ( imageData.PixelFormat == PixelFormat.Format8bppIndexed ) ? 1 : 3;
 
-			int lineSize = width * ( ( imageData.PixelFormat == PixelFormat.Format8bppIndexed ) ? 1 : 3 );
-			int offset = imageData.Stride - lineSize;
+            int startY  = rect.Top;
+            int stopY   = startY + rect.Height;
 
-			// do the job
-			byte * ptr = (byte *) imageData.Scan0.ToPointer( );
+            int startX  = rect.Left * pixelSize;
+            int stopX   = startX + rect.Width * pixelSize;
 
-			// invert
-			for ( int y = 0; y < height; y++ )
-			{
-				for ( int x = 0; x < lineSize; x++, ptr ++ )
-				{
-					// ivert each pixel
-					*ptr = (byte)( 255 - *ptr );
-				}
-				ptr += offset;
-			}
-		}
-	}
+            int offset  = imageData.Stride - ( stopX - startX );
+
+            // do the job
+            byte* ptr = (byte*) imageData.Scan0.ToPointer( );
+
+            // allign pointer to the first pixel to process
+            ptr += ( startY * imageData.Stride + rect.Left * pixelSize );
+
+            // invert
+            for ( int y = startY; y < stopY; y++ )
+            {
+                for ( int x = startX; x < stopX; x++, ptr++ )
+                {
+                    // ivert each pixel
+                    *ptr = (byte) ( 255 - *ptr );
+                }
+                ptr += offset;
+            }
+        }
+    }
 }
