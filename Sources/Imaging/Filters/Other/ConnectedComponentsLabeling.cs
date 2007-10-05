652cdcbe9ab28e759c3415d78f7b99113f718397
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2007-10-05 06:06:36 +0000

    Connected Components Labeling filter updated with some XML documentation.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@361 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Imaging/Filters/Other/ConnectedComponentsLabeling.cs
+++ Sources/Imaging/Filters/Other/ConnectedComponentsLabeling.cs
@@ -1,26 +1,41 @@
 // AForge Image Processing Library
+// AForge.NET framework
 //
-// Copyright © Andrew Kirillov, 2005-2006
+// Copyright © Andrew Kirillov, 2005-2007
 // andrew.kirillov@gmail.com
 //
 
 namespace AForge.Imaging.Filters
 {
-	using System;
-	using System.Drawing;
-	using System.Drawing.Imaging;
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
 
-	/// <summary>
-	/// Connected components labeling
-	/// </summary>
-	/// 
-	/// <remarks>The filter performs labeling of objects in binary image. It colors
-	/// each separate object using different color.</remarks>
-	/// 
-	public class ConnectedComponentsLabeling : FilterGrayToColor
-	{
-		// Color table for coloring objects
-		private static Color[] colorTable = new Color[]
+    /// <summary>
+    /// Connected components labeling.
+    /// </summary>
+    /// 
+    /// <remarks><para>The filter performs labeling of objects in binary image. It colors
+    /// each separate object using different color.</para>
+    /// <para>Sample usage:</para>
+    /// <code>
+    /// // create filter
+    /// ConnectedComponentsLabeling filter = new ConnectedComponentsLabeling( );
+    /// // apply the filter
+    /// filter.Apply( image );
+    /// // check objects count
+    /// int objectCount = filter.ObjectCount;
+    /// </code>
+    /// <para><b>Initial image:</b></para>
+    /// <img src="sample2.jpg" width="320" height="240" />
+    /// <para><b>Result image:</b></para>
+    /// <img src="labeling.jpg" width="320" height="240" />
+    /// </remarks>
+    /// 
+    public class ConnectedComponentsLabeling : FilterGrayToColor
+    {
+        // Color table for coloring objects
+        private static Color[] colorTable = new Color[]
 		{
 			Color.Red,		Color.Green,	Color.Blue,			Color.Yellow,
 			Color.Violet,	Color.Brown,	Color.Olive,		Color.Cyan,
@@ -35,68 +50,71 @@ namespace AForge.Imaging.Filters
             Color.PowderBlue, Color.Plum,	Color.PapayaWhip,	Color.Orange
 		};
 
-		// blob counter
-		private BlobCounter blobCounter = new BlobCounter( );
+        // blob counter
+        private BlobCounter blobCounter = new BlobCounter( );
 
-		/// <summary>
-		/// Colors used to color the binary image
-		/// </summary>
-		public static Color[] ColorTable
-		{
-			get { return colorTable; }
-			set { colorTable = value; }
-		}
+        /// <summary>
+        /// Colors used to color the binary image.
+        /// </summary>
+        public static Color[] ColorTable
+        {
+            get { return colorTable; }
+            set { colorTable = value; }
+        }
 
-		/// <summary>
-		/// Objects count
-		/// </summary>
-		public int ObjectCount
-		{
-			get { return blobCounter.ObjectsCount; }
-		}
+        /// <summary>
+        /// Objects count.
+        /// </summary>
+        /// 
+        /// <remarks>The amount of objects found in the last processed image.</remarks>
+        /// 
+        public int ObjectCount
+        {
+            get { return blobCounter.ObjectsCount; }
+        }
 
-		/// <summary>
-		/// Process the filter on the specified image
-		/// </summary>
-		/// 
-		/// <param name="sourceData">Source image data</param>
-		/// <param name="destinationData">Destination image data</param>
-		/// 
-		protected override unsafe void ProcessFilter( BitmapData sourceData, BitmapData destinationData )
-		{
-			// process the image
-			blobCounter.ProcessImage( sourceData );
+        /// <summary>
+        /// Process the filter on the specified image.
+        /// </summary>
+        /// 
+        /// <param name="sourceData">Source image data.</param>
+        /// <param name="destinationData">Destination image data.</param>
+        /// 
+        protected override unsafe void ProcessFilter( BitmapData sourceData, BitmapData destinationData )
+        {
+            // process the image
+            blobCounter.ProcessImage( sourceData );
 
-			// get object labels
-			int[] labels = blobCounter.ObjectLabels;
+            // get object labels
+            int[] labels = blobCounter.ObjectLabels;
 
-			// get width and height
-			int width = sourceData.Width;
-			int height = sourceData.Height;
+            // get width and height
+            int width = sourceData.Width;
+            int height = sourceData.Height;
 
-			int dstOffset = destinationData.Stride - width * 3;
+            int dstOffset = destinationData.Stride - width * 3;
 
-			// do the job
-			byte *	dst = (byte *) destinationData.Scan0.ToPointer( );
-			int		p = 0;
+            // do the job
+            byte* dst = (byte*) destinationData.Scan0.ToPointer( );
+            int p = 0;
 
-			// for each row
-			for ( int y = 0; y < height; y++ )
-			{
-				// for each pixel
-				for ( int x = 0; x < width; x++, dst += 3, p++ )
-				{
-					if ( labels[p] != 0 )
-					{
-						Color c = colorTable[( labels[p] - 1 ) % colorTable.Length];
+            // for each row
+            for ( int y = 0; y < height; y++ )
+            {
+                // for each pixel
+                for ( int x = 0; x < width; x++, dst += 3, p++ )
+                {
+                    if ( labels[p] != 0 )
+                    {
+                        Color c = colorTable[( labels[p] - 1 ) % colorTable.Length];
 
-						dst[RGB.R] = c.R;
-						dst[RGB.G] = c.G;
-						dst[RGB.B] = c.B;
-					}
-				}
-				dst += dstOffset;
-			}
-		}
-	}
+                        dst[RGB.R] = c.R;
+                        dst[RGB.G] = c.G;
+                        dst[RGB.B] = c.B;
+                    }
+                }
+                dst += dstOffset;
+            }
+        }
+    }
 }
