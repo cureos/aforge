25e353a456fedcc97318d70f8748f42bdac85745
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2008-06-30 22:01:38 +0000

    Code review completed for AForge.Imaging.Drawing.Line() function. The code is merged into AForge.Imaging namespace.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@563 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Imaging/Drawing.cs
+++ Sources/Imaging/Drawing.cs
@@ -251,5 +251,193 @@ namespace AForge.Imaging
                 }
             }
         }
+
+        /// <summary>
+        /// Draw a line on the specified image.
+        /// </summary>
+        /// 
+        /// <param name="imageData">Source image data.</param>
+        /// <param name="point1">The first point to connect.</param>
+        /// <param name="point2">The second point to connect.</param>
+        /// <param name="color">Line's color.</param>
+        /// 
+        public static unsafe void Line( BitmapData imageData, Point point1, Point point2, Color color )
+        {
+            // TODO: faster line drawing algorithm may be implemented with integer math
+
+            // check pixel format
+            if (
+                ( imageData.PixelFormat != PixelFormat.Format24bppRgb ) &&
+                ( imageData.PixelFormat != PixelFormat.Format8bppIndexed )
+                )
+            {
+                throw new ArgumentException( "Source image can be graysclae (8 bpp indexed) or color (24 bpp) image only" );
+            }
+
+            // image dimension
+            int imageWidth = imageData.Width;
+            int imageHeight = imageData.Height;
+            int stride = imageData.Stride;
+
+            // check if there is something to draw
+            if (
+                ( ( point1.X < 0 ) && ( point2.X < 0 ) ) ||
+                ( ( point1.Y < 0 ) && ( point2.Y < 0 ) ) ||
+                ( ( point1.X >= imageWidth ) && ( point2.X >= imageWidth ) ) ||
+                ( ( point1.Y >= imageHeight ) && ( point2.Y >= imageHeight ) ) )
+            {
+                // nothing to draw
+                return;
+            }
+
+            CheckEndPoint( imageWidth, imageHeight, point1, ref point2 );
+            CheckEndPoint( imageWidth, imageHeight, point2, ref point1 );
+
+            // check again if there is something to draw
+            if (
+                ( ( point1.X < 0 ) && ( point2.X < 0 ) ) ||
+                ( ( point1.Y < 0 ) && ( point2.Y < 0 ) ) ||
+                ( ( point1.X >= imageWidth ) && ( point2.X >= imageWidth ) ) ||
+                ( ( point1.Y >= imageHeight ) && ( point2.Y >= imageHeight ) ) )
+            {
+                // nothing to draw
+                return;
+            }
+
+            int startX = point1.X;
+            int startY = point1.Y;
+            int stopX  = point2.X;
+            int stopY  = point2.Y;
+      
+            // compute pixel for grayscale image
+            byte gray = 0;
+            if ( imageData.PixelFormat == PixelFormat.Format8bppIndexed )
+            {
+                gray = (byte)( 0.2125 * color.R + 0.7154 * color.G + 0.0721 * color.B );
+            }
+ 
+            // draw the line
+            int dx = stopX - startX;
+            int dy = stopY - startY;
+
+            if ( Math.Abs( dx ) >= Math.Abs( dy ) )
+            {
+                // the line is more horizontal, we'll plot along the X axis
+                float slope = (float) dy / (float) dx;
+                int step = ( dx > 0 ) ? 1 : -1;
+
+                // correct dx so last point is included as well
+                dx += step;
+
+                if ( imageData.PixelFormat == PixelFormat.Format8bppIndexed )
+                {
+                    // grayscale image
+                    for ( int x = 0; x != dx; x += step )
+                    {
+                        int px = startX + x;
+                        int py = (int) ( (float) startY + ( slope * (float) x ) );
+
+                        byte* ptr = (byte*) imageData.Scan0.ToPointer( ) + py * stride + px;
+                        *ptr = gray;
+                    }
+                }
+                else
+                {
+                    // color image
+                    for ( int x = 0; x != dx; x += step )
+                    {
+                        int px = startX + x;
+                        int py = (int) ( (float) startY + ( slope * (float) x ) );
+
+                        byte* ptr = (byte*) imageData.Scan0.ToPointer( ) + py * stride + px * 3;
+
+                        ptr[RGB.R] = color.R;
+                        ptr[RGB.G] = color.G;
+                        ptr[RGB.B] = color.B;
+                    }
+                }
+            }
+            else
+            {
+                // the line is more vertical, we'll plot along the y axis.
+                float slope = (float) dx / (float) dy;
+                int step = ( dy > 0 ) ? 1 : -1;
+
+                // correct dy so last point is included as well
+                dy += step;
+
+                if ( imageData.PixelFormat == PixelFormat.Format8bppIndexed )
+                {
+                    // grayscale image
+                    for ( int y = 0; y != dy; y += step )
+                    {
+                        int px = (int) ( (float) startX + ( slope * (float) y ) );
+                        int py = startY + y;
+
+                        byte* ptr = (byte*) imageData.Scan0.ToPointer( ) + py * stride + px;
+                        *ptr = gray;
+                    }
+                }
+                else
+                {
+                    // color image
+                    for ( int y = 0; y != dy; y += step )
+                    {
+                        int px = (int) ( (float) startX + ( slope * (float) y ) );
+                        int py = startY + y;
+
+                        byte* ptr = (byte*) imageData.Scan0.ToPointer( ) + py * stride + px * 3;
+
+                        ptr[RGB.R] = color.R;
+                        ptr[RGB.G] = color.G;
+                        ptr[RGB.B] = color.B;
+                    }
+                }
+            }
+        }
+
+
+        // Check end point and make sure it is in the image
+        private static void CheckEndPoint( int width, int height, Point start, ref Point end )
+        {
+            if ( end.X >= width )
+            {
+                int newEndX = width - 1;
+
+                double c = (double) ( newEndX - start.X ) / ( end.X - start.X );
+
+                end.Y = (int) ( start.Y + c * ( end.Y - start.Y ) );
+
+                end.X = newEndX;
+            }
+
+            if ( end.Y >= height )
+            {
+                int newEndY = height - 1;
+
+                double c = (double) ( newEndY - start.Y ) / ( end.Y - start.Y );
+
+                end.X = (int) ( start.X + c * ( end.X - start.X ) );
+
+                end.Y = newEndY;
+            }
+
+            if ( end.X < 0 )
+            {
+                double c = (double) ( 0 - start.X ) / ( end.X - start.X );
+
+                end.Y = (int) ( start.Y + c * ( end.Y - start.Y ) );
+                end.X = 0;
+            }
+
+            if ( end.Y < 0 )
+            {
+                double c = (double) ( 0 - start.Y ) / ( end.Y - start.Y );
+
+                end.X = (int) ( start.X + c * ( end.X - start.X ) );
+
+                end.Y = 0;
+            }
+        }
     }
 }
