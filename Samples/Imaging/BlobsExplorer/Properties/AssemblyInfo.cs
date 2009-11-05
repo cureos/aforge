07dc05080b6d457b30dc9e57553b48dbb916614e
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2009-11-05 10:25:17 +0000

    Updating Assembly Info for Blobs Explorer sample.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@1129 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Samples/Imaging/BlobsExplorer/Properties/AssemblyInfo.cs
+++ Samples/Imaging/BlobsExplorer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "WindowsFormsApplication1" )]
+[assembly: AssemblyTitle( "Blobs Explorer" )]
 [assembly: AssemblyDescription( "" )]
 [assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "WindowsFormsApplication1" )]
-[assembly: AssemblyCopyright( "Copyright ©  2009" )]
+[assembly: AssemblyCompany( "AForge" )]
+[assembly: AssemblyProduct( "Blobs Explorer" )]
+[assembly: AssemblyCopyright( "AForge.NET © 2009" )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
