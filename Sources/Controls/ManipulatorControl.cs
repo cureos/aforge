9e1b72ed4b80777a02574eb8d28454ae1826dd47
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2010-02-08 17:58:52 +0000

    Updating documentation for ManipulatorColor class.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@1165 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Controls/ManipulatorControl.cs
+++ Sources/Controls/ManipulatorControl.cs
@@ -26,13 +26,13 @@ namespace AForge.Controls
     /// <para>For example, in robotics applications the control can be used to drive robots. If user drags manipulator
     /// further from centre (increasing distance between centre and manipulator), then higher power (speed) should be
     /// set for robot's motors. But dragging it in different directions away from centre should result in changing
-    /// robot's direction: straight forward, backward, turning right or left, etc.
+    /// robot's direction: straight forward, backward, turning right or left, etc.<br />
     /// <img src="img/controls/manipulator_round.png" width="215" height="215" />
     /// </para>
     /// 
     /// <para>Another possible application of the control is to control position of some device, etc.
     /// For example, the control could be used with pan-tilt camera - by dragging control away from centre,
-    /// the camera may rotate in one of the directions.
+    /// the camera may rotate in one of the directions.<br />
     /// <img src="img/controls/manipulator_square.png" width="215" height="215" />
     /// </para>
     /// </remarks>
