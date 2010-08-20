96c21c823af6c40cb9d1f568d0c05aa6cbeb155b
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2010-08-20 08:10:35 +0000

    Fixing typo in comment.
    git-svn-id: http://aforge.googlecode.com/svn/trunk@1299 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

--- Sources/Video.DirectShow/FileVideoSource.cs
+++ Sources/Video.DirectShow/FileVideoSource.cs
@@ -514,7 +514,7 @@ namespace AForge.Video.DirectShow
         }
 
         //
-        // Vodeo grabber
+        // Video grabber
         //
         private class Grabber : ISampleGrabberCB
         {
