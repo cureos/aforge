b74ad7ecc6b10aa8f5802d212153e93d785eb049
andrew.kirillov <andrew.kirillov@ab5b2c17-5f1b-0410-93cd-b5c26ce635c7>
2010-11-12 13:39:26 +0000

    Issue 169: Distance and Similarity metrics
    Committing contributed code, which is about distance and similarity metrics. There are still few issues to be resolved, which are described in issues tacking system.
    
    git-svn-id: http://aforge.googlecode.com/svn/trunk@1340 ab5b2c17-5f1b-0410-93cd-b5c26ce635c7

---
+++ Sources/Math/Metrics/CosineDistance.cs
@@ -0,0 +1,69 @@
+﻿// AForge Math Library
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2007-2010
+// contacts@aforgenet.com
+//
+
+namespace AForge.Math.Metrics
+{
+    using System;
+
+    /// <summary>
+    /// Cosine distance metric. 
+    /// </summary>
+    /// 
+    /// <remarks><para>This class represents the 
+    /// <a href="http://en.wikipedia.org/wiki/Cosine_similarity">Cosine distance metric</a>.</para>
+    /// 
+    /// <para>Sample usage:</para>
+    /// <code>
+    /// // instantiate new distance class
+    /// CosineDistance dist = new CosineDistance(); 
+    /// // create two vectors for inputs
+    /// double[] p = new double[] { 2.5, 3.5, 3.0, 3.5, 2.5, 3.0 };
+    /// double[] q = new double[] { 3.0, 3.5, 1.5, 5.0, 3.5, 3.0 };
+    /// // get distance between the two vectors
+    /// double distance = dist.GetDistance( p, q );
+    /// </code>
+    /// </remarks>
+    /// 
+    public sealed class CosineDistance : IDistance
+    {
+        /// <summary>
+        /// Returns distance between two N-dimensional double vectors.
+        /// </summary>
+        /// 
+        /// <param name="p">1st point vector.</param>
+        /// <param name="q">2nd point vector.</param>
+        /// 
+        /// <returns>Returns Cosine distance between two supplied vectors.</returns>
+        /// 
+        /// <exception cref="ArgumentException">Thrown if the two vectors are of different dimensions (if specified
+        /// array have different length).</exception>
+        /// 
+        public double GetDistance( double[] p, double[] q )
+        {
+            double denominator = 0, numerator = 0, pSumSq = 0, qSumSq = 0;
+            double pValue, qValue;
+
+            if ( p.Length != q.Length )
+                throw new ArgumentException( "Input vectors must be of the same dimension." );
+
+            for ( int x = 0, len = p.Length; x < len; x++ )
+            {
+                pValue = p[x];
+                qValue = q[x];
+
+                numerator += pValue * qValue;
+                pSumSq    += pValue * pValue;
+                qSumSq    += qValue * qValue;
+            }
+
+            denominator = Math.Sqrt( pSumSq ) * Math.Sqrt( qSumSq );
+
+            return numerator / denominator;
+        }
+    }
+}
---
+++ Sources/Math/Metrics/CosineSimilarity.cs
@@ -0,0 +1,56 @@
+﻿// AForge Math Library
+// AForge.NET framework
+// http://www.aforgenet.com/framework/
+//
+// Copyright © AForge.NET, 2007-2010
+// contacts@aforgenet.com
+//
+
+namespace AForge.Math.Metrics
+{
+    using System;
+
+    /// <summary>
+    /// Cosine similarity metric. 
+    /// </summary>
+    /// 
+    /// <remarks><para>This class represents the 
+    /// <a href="http://en.wikipedia.org/wiki/Cosine_similarity">Cosine Similarity metric</a>.</para>
+    /// 
+    /// <para>Sample usage:</para>
+    /// <code>
+    /// // instantiate new similarity class
+    /// CosineSimilarity sim = new CosineSimilarity( ); 
+    /// // create two vectors for inputs
+    /// double[] p = new double[] { 2.5, 3.5, 3.0, 3.5, 2.5, 3.0 };
+    /// double[] q = new double[] { 3.0, 3.5, 1.5, 5.0, 3.5, 3.0 };
+    /// // get simirarity between the two vectors
+    /// double similarityScore = sim.GetSimilarityScore( p, q );
+    /// </code>    
+    /// </remarks>
+    /// 
+    public sealed class CosineSimilarity : ISimilarity
+    {
+        /// <summary>
+        /// Returns similarity score for two N-dimensional double vectors. 
+        /// </summary>
+        /// 
+        /// <param name="p">1st point vector.</param>
+        /// <param name="q">2nd point vector.</param>
+        /// 
+        /// <returns>Returns Cosine similarity between two supplied vectors.</returns>
+        /// 
+        /// <exception cref="ArgumentException">Thrown if the two vectors are of different dimensions (if specified
+        /// array have different length).</exception>
+        /// 
+        public double GetSimilarityScore( double[] p, double[] q )
+        {
+            double similarity;
+
+            CosineDistance dist = new CosineDistance( );
+            similarity = (double) 1 - dist.GetDistance( p, q );
+
+            return similarity;
+        }
+    }
+}
