3d4a3189d6a4e85102d3c425ea25071f254104eb
Anders Gustafsson <anders@cureos.com>
2013-12-29 18:51:38 +0100

    Refactored implementation to enable accurate replacement with real types

--- Sources/System/Collections/Concurrent/ConcurrentStack.cs
+++ Sources/System/Collections/Concurrent/ConcurrentStack.cs
@@ -9,7 +9,71 @@ using System.Collections.Generic;
 
 namespace System.Collections.Concurrent
 {
-    public sealed class ConcurrentStack<T> : Stack<T>
-    {
-    }
+	public sealed class ConcurrentStack<T> : IEnumerable<T>, ICollection
+	{
+		#region FIELDS
+
+		private readonly Stack<T> _stack;
+		private readonly object _syncRoot;
+ 
+		#endregion
+
+		#region CONSTRUCTORS
+
+		public ConcurrentStack()
+		{
+			_stack = new Stack<T>();
+			_syncRoot = new object();
+		}
+
+		#endregion
+		
+		#region PROPERTIES
+
+		public int Count
+		{
+			get { return _stack.Count; }
+		}
+
+		public bool IsSynchronized
+		{
+			get { return true; }
+		}
+
+		public object SyncRoot
+		{
+			get { return _syncRoot; }
+		}
+
+		#endregion
+
+		#region METHODS
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return _stack.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public void CopyTo(Array array, int index)
+		{
+			throw new NotImplementedException();
+		}
+
+		public T[] ToArray()
+		{
+			return _stack.ToArray();
+		}
+
+		public void Push(T item)
+		{
+			_stack.Push(item);
+		}
+
+		#endregion
+	}
 }
