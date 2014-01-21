eec6280568a23642fda7089c0153b6ec7081b141
Anders Gustafsson <anders@cureos.com>
2014-01-21 16:27:12 +0100

    Implemented FromStream and corrected cast to BitmapSource

--- Sources/System.Drawing/_Store/Bitmap.Store.cs
+++ Sources/System.Drawing/_Store/Bitmap.Store.cs
@@ -12,6 +12,9 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
 using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
@@ -23,39 +26,50 @@ namespace System.Drawing
 
 		public static Bitmap FromStream(Stream stream)
 		{
-            throw new NotImplementedException();
-/*            var bitmapImage = new BitmapImage();
-            bitmapImage.SetSource(stream.AsInputStream());
-            bitmapImage.CreateOptions = BitmapCreateOptions.None;
+		    Bitmap bitmap = null;
 
-            var writeableBitmap = new WriteableBitmap(bitmapImage);
-            bitmapImage.UriSource = null;
+		    Task.Run(async () =>
+		    {
+		        using (var raStream = new InMemoryRandomAccessStream())
+		        {
+		            await stream.CopyToAsync(raStream.AsStream());
+		            var decoder = await BitmapDecoder.CreateAsync(raStream);
+		            var pixelData = await decoder.GetPixelDataAsync();
 
-            return writeableBitmap;*/
+		            var width = (int)decoder.OrientedPixelWidth;
+		            var height = (int)decoder.OrientedPixelHeight;
+		            const PixelFormat format = PixelFormat.Format32bppArgb;
+		            var bytes = pixelData.DetachPixelData();
+
+                    bitmap = new Bitmap(width, height, format);
+                    var data = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, format);
+                    Marshal.Copy(bytes, 0, data.Scan0, bytes.Length);
+                    bitmap.UnlockBits(data);
+                }
+		    }).Wait();
+
+		    return bitmap;
 		}
 
 		#endregion
 
 		#region OPERATORS
 
-		public static implicit operator WriteableBitmap(Bitmap bitmap)
-		{
-			if (bitmap.PixelFormat != PixelFormat.Format32bppPArgb)
-				bitmap = bitmap.Clone(PixelFormat.Format32bppPArgb);
+	    public static implicit operator BitmapSource(Bitmap bitmap)
+	    {
+	        if (bitmap.PixelFormat != PixelFormat.Format32bppPArgb)
+	            bitmap = bitmap.Clone(PixelFormat.Format32bppPArgb);
 
-			var bytes = new byte[bitmap._stride * bitmap._height];
-			Marshal.Copy(bitmap._scan0, bytes, 0, bytes.Length);
+	        var bytes = new byte[bitmap._stride * bitmap._height];
+	        Marshal.Copy(bitmap._scan0, bytes, 0, bytes.Length);
 
-			var writeableBitmap = new WriteableBitmap(bitmap._width, bitmap._height);
-		    using (var stream = writeableBitmap.PixelBuffer.AsStream())
-		    {
-		        stream.Write(bytes, 0, bytes.Length);
-		    }
+	        var bitmapImage = new WriteableBitmap(bitmap._width, bitmap._height);
+	        bitmapImage.PixelBuffer.AsStream().Write(bytes, 0, bytes.Length);
 
-			return writeableBitmap;
-		}
+	        return bitmapImage;
+	    }
 
-		public static implicit operator Bitmap(WriteableBitmap writeableBitmap)
+	    public static implicit operator Bitmap(WriteableBitmap writeableBitmap)
 		{
 			var width = writeableBitmap.PixelWidth;
 			var height = writeableBitmap.PixelHeight;
