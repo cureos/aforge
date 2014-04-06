d6835a48ac75af25b44d2ac40de56f42a56b4085
Anders Gustafsson <anders@cureos.com>
2014-04-06 21:10:48 +0200

    Added copyright notices on Smart K8 code. Issue #8

--- Sources/System.Drawing/Quantizers/BaseColorCacheQuantizer.cs
+++ Sources/System.Drawing/Quantizers/BaseColorCacheQuantizer.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ *  This file is adapted from Code Project article "Image Per Pixel Enumeration, Pixel Format Conversion and More"
+ *  Copyright (c) 2010-2012 Smart K8
+ *  http://www.codeproject.com/Articles/66550/Image-Per-Pixel-Enumeration-Pixel-Format-Conversio
+ *  
+ *  Released under Code Project Open License, CPOL, http://www.codeproject.com/info/cpol10.aspx
+ */
+
+using System;
 using System.Drawing;
 using System.Collections.Generic;
 using ImagePixelEnumerator.ColorCaches;
