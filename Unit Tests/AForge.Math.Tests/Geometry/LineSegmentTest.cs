6126e2452fef38cc5569697c6cd1c093c2f3b3b5
Anders Gustafsson <anders@cureos.com>
2015-01-11 16:00:35 +0100

    Synched unit tests with main AForge on Github

--- Unit Tests/AForge.Math.Tests/Geometry/LineSegmentTest.cs
+++ Unit Tests/AForge.Math.Tests/Geometry/LineSegmentTest.cs
@@ -119,11 +119,10 @@ namespace AForge.Math.Geometry.Tests
             Assert.AreEqual( null, segB.GetIntersectionWith( segA ) );
             Assert.AreEqual( null, segA.GetIntersectionWith( segB ) );
         }
-
-/*        
+       
         [TestCase( 0, 0, 1, 1, 2, 2, 3, 3 )]
         [TestCase( 0, 1, 0, 2, 0, 3, 0, 4 )]
-        [TestCase( 0, 0, -1, 1, -2, 2, -3, 3, -4, 4 )]
+        [TestCase( 0, 0, -1, 1, -2, 2, -3, 3 )]
         [TestCase( 1, 0, 2, 0, 3, 0, 4, 0 )]
         [TestCase(0, 0, 0, 1, 0, 2, 0, 3 )]
         public void CollinearIntersectionPointTest(float ax1, float ay1, float ax2, float ay2, float bx1, float by1, float bx2, float by2)
@@ -141,7 +140,6 @@ namespace AForge.Math.Geometry.Tests
             Assert.AreEqual( null, segB.GetIntersectionWith( segA ) );
             Assert.AreEqual( null, segA.GetIntersectionWith( segB ) );
         }
-
         
         [TestCase( 0, 0, 1, 1, 1, 1, 3, 3, 1, 1 )]
         [TestCase( 0, 0, 1, 1, 3, 3, 1, 1, 1, 1 )]
@@ -187,5 +185,5 @@ namespace AForge.Math.Geometry.Tests
             Assert.Throws<InvalidOperationException>( ( ) => segB.GetIntersectionWith( segA ) );
             Assert.Throws<InvalidOperationException>( ( ) => segA.GetIntersectionWith( segB ) );
         }
-*/    }
+    }
 }
