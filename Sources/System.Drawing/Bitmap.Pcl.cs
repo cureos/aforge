1480f1f7658faa85be13524ec37a0255d10e5792
Anders Gustafsson <anders@cureos.com>
2015-01-22 14:35:51 +0100

    Updated copyright year and changed Shim.NET to Shim.Drawing

--- Sources/System.Drawing/Bitmap.Pcl.cs
+++ Sources/System.Drawing/Bitmap.Pcl.cs
@@ -1,22 +1,22 @@
 ﻿/*
- *  Copyright (c) 2013-2014, Cureos AB.
+ *  Copyright (c) 2013-2015, Cureos AB.
  *  All rights reserved.
  *  http://www.cureos.com
  *
- *	This file is part of Shim.NET.
+ *	This file is part of Shim.Drawing.
  *
- *  Shim.NET is free software: you can redistribute it and/or modify
+ *  Shim.Drawing is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Shim.NET is distributed in the hope that it will be useful,
+ *  Shim.Drawing is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Shim.NET.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Shim.Drawing.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System.IO;
