cb28c72883b201e27e51c9f78895113323bf1b1f
Anders Gustafsson <anders@cureos.com>
2015-05-04 08:49:37 +0200

    Updated AssemblyInfo version numbers

--- Sources/Video/Properties/AssemblyInfo.cs
+++ Sources/Video/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion( "2.2.5.0" )]
-[assembly: AssemblyFileVersion( "2.2.5.0" )]
+[assembly: AssemblyFileVersion( "2.2.5.50504" )]
